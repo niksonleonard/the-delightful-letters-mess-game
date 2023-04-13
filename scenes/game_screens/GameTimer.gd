12b62e6f362f26bcda3fa6db6da5699339cc0de0
Nikson Leonard <niksonleonar@gmail.com>
2023-04-12 23:56:02 -0300

    refactor: countdown timer improved

--- scenes/game_screens/GameTimer.gd
+++ scenes/game_screens/GameTimer.gd
@@ -1,17 +1,7 @@
 extends Timer
 @export var hourlass: Label
-var countdown = 30
-var timer = 1.0 # tempo em segundos para cada contagem regressiva
-var current_time = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	current_time += delta
-	hourlass.text = str(countdown)
-	if current_time >= timer:
-		current_time = 0.0
-		countdown -= 1
-
+func _process(_delta):
+	# The Timer node already has an property named "time_left" we can only get 
+	# the Integer part of it value and set it to or Countdown Label =]
+	hourlass.text = str(int(time_left))
