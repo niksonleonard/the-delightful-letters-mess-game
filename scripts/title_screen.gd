6733b452375072f36f71e9e6343a518cf03a8100
Nikson Leonard <niksonleonar@gmail.com>
2023-04-14 00:11:31 -0300

    feat: title screen to album flow

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
-func _on_button_pressed():
+func _on_button_startgame_pressed():
 	get_tree().change_scene_to_file("res://scenes/game_screens/main_screen.tscn")
 	print("Starting game")
+
+func _on_button_album_pressed():
+	get_tree().change_scene_to_file("res://scenes/game_screens/stamp_album_screen.tscn")
+	print("Album screen")
